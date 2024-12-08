74f163ece4548f4de08e6c50c139b1e8c2cc6d54
donCallm <nikitich-1998@bk.ru>
2024-12-09 02:32:26 +0300

    allocs

--- allocators/linear_allocator.hpp
+++ allocators/linear_allocator.hpp
@@ -1,93 +1,55 @@
-#pragma once
-
-#include <iostream>
 #include <new>
 #include <cstddef>
 
-template <typename T>
+constexpr const size_t default_capacity = 100;
+
+template <class T>
 struct linear_alloc {
-    typedef size_t    size_type;
-    typedef ptrdiff_t difference_type;
-    typedef T*        pointer;
-    typedef const T*  const_pointer;
-    typedef T&        reference;
-    typedef const T&  const_reference;
-    typedef T         value_type;
-    
-    template <class U>
-    struct rebind {
-        typedef linear_alloc<U> other; };
+    using value_type = T;
 
-    linear_alloc()
-        : _buf(nullptr),
-        _capacity(0),
-        _size(0)
+    linear_alloc(size_t capacity = default_capacity)
+        : _buf(new value_type[capacity]),
+        _size(0),
+        _capacity(capacity)
     {}
 
-    linear_alloc(size_t capacity)
-        : _buf(reinterpret_cast<T*>(new char [sizeof(T) * capacity])),
-        _capacity(capacity),
-        _size(0)
-    {}
-
-    linear_alloc(linear_alloc<T>&& other)
-        : _buf(other._buf),
-        _capacity(other._capacity),
-        _size(other._size)
-    {
-        other._buf = nullptr;
-        other._capacity = 0;
-        other._size = 0;
-    }
-    
-    pointer allocate(size_t count) {
-        if (count > _capacity || _size + count > _capacity) {
-            throw std::runtime_error("there is not enough free memory in the buffer");
+    ~linear_alloc() {
+        if (_buf) {
+            delete[] _buf;
         }
+    }
 
+public:
+    value_type* allocate(size_t count) {
+        if (count > (_capacity - _size)) {
+            return new value_type[count];
+        }
         _size += count;
         return _buf + (_size - count);
     }
 
-    void deallocate(pointer, size_t count) {
-        if (_size < count)
-            throw std::out_of_range("count > _capacity");
-        _size -= count;
+    void deallocate(value_type*& ptr, size_t) {
+        if (ptr) {
+            if (ptr < _buf || ptr > _buf + _capacity)
+                delete[] ptr;
+            ptr = nullptr;
+        }
     }
 
-    template <typename U, typename ...Args>
-    void construct(U*, Args&&... args) {
-        if (_size == _capacity)
-            throw std::out_of_range("capacity is over");
-        new(_buf + ++_size) U(args...);
+    template <typename ...Args>
+    void construct(value_type* ptr, Args&&... args) {
+        *ptr = value_type(std::forward<Args>(args)...);
     }
 
-    template <typename U>
-    void destruct(U*, size_t index = 0) {
-        if (index > _size)
-            throw std::out_of_range("index biger then size");
-        if (_size == 0)
-            throw std::runtime_error("buffer is empty");
-
-        --_size;
-
-        if (index != 0)
-            std::swap((_buf + index), (_buf + _size));
+    void destroy(value_type* ptr) { 
+        ptr->~value_type();
     }
 
-    ~linear_alloc() {
-        print_size();
-        if (_buf == nullptr)
-            return;
-        delete[] _buf;
-    }
+    size_t size() { return _size; }
+    size_t capacity() { return _capacity; }
 
-    void print_size() {
-        std::cout << "SIZE: " << _size << '\n';
-    }
-    
 private:
-    pointer _buf;
-    size_t _capacity;
+    value_type* _buf;
     size_t _size;
+    size_t _capacity;
 };
--- allocators/main.cpp
+++ allocators/main.cpp
@@ -1,84 +1,49 @@
 #include <iostream>
+#include <cassert>
+#include <vector>
 #include "linear_allocator.hpp"
 
 void linear_alloc_test() {
     std::cout << "-- LINEAR ALLOC --\n\n";
 
-    std::cout << "test allocate:\n";
-    linear_alloc<int> empty_alloc;
-    linear_alloc<int> normal_alloc(100);
-    try {
-        empty_alloc.allocate(1);
-        std::cout << "empty_alloc::alloc - ERR\n";
-    } catch(const std::exception& e) {
-        std::cout << "empty_alloc::alloc - SUCCESS\n";
-    }
-    
-    try {
-        auto ptr = normal_alloc.allocate(1);
-        std::cout << "normal_alloc::alloc - SUCCESS\n";
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        std::cout << "normal_alloc::alloc - ERR\n";
+    {
+        std::cout << "test сapacity: ";
+        linear_alloc<int> empty_alloc;
+        linear_alloc<int> normal_alloc(100);
+        assert(empty_alloc.capacity() == normal_alloc.capacity());
+        std::cout << "SUCCESS\n";
     }
 
-    try {
-        for (size_t i = 0; i < 99; ++i)
-            auto ptr = normal_alloc.allocate(1);
-        
-        std::cout << "normal_alloc::alloc_with_for - SUCCESS\n";
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        std::cout << "normal_alloc::alloc - ERR\n";
+    {
+        std::cout << "test allocate: ";
+        linear_alloc<int> alloc;
+        int* ptr1 = alloc.allocate(50);
+        assert(alloc.size() == 50);
+
+        int* ptr2 = alloc.allocate(100);
+        assert(alloc.size() == 50);
+
+        std::cout << "SUCCESS\n";
     }
 
-    try {
-        for (size_t i = 0; i < 10; ++i)
-            auto ptr = normal_alloc.allocate(1);
-        std::cout << "normal_alloc::alloc - ERR\n";
-    } catch(const std::exception& e) {
-        std::cout << "normal_alloc::alloc_with_full_buf - SUCCESS\n";
-    }
+    {
+        std::cout << "test deallocate: ";
 
-    std::cout << "test deallocate:\n";
-    try {
-        int* temp;
-        empty_alloc.deallocate(temp, 1);
-        std::cout << "empty_alloc::dealloc - ERR\n";
-    } catch(const std::exception& e) {
-        std::cout << "empty_alloc::dealloc - SUCCESS\n";
-    }
+        linear_alloc<int> alloc;
+        int* ptr = alloc.allocate(150);
+        alloc.deallocate(ptr, 150);
+        assert(ptr == nullptr);
 
-    try {
-        int* temp;
-        normal_alloc.deallocate(temp, 1);
-        std::cout << "normal_alloc::dealloc - SUCCESS\n";
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        std::cout << "normal_alloc::daalloc - ERR\n";
-    }
-
-    try {
-        int* temp;
-        for (size_t i = 0; i < 99; ++i)
-            normal_alloc.deallocate(temp, 1);
-        std::cout << "normal_alloc::dealloc_with_for - SUCCESS\n";
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        std::cout << "normal_alloc::alloc - ERR\n";
-    }
-
-    try {
-        int* temp;
-        normal_alloc.deallocate(temp, 1);
-        std::cout << "normal_alloc::dealloc_after_for - ERR\n";
-    } catch(const std::exception& e) {
-        std::cout << "normal_alloc::dealloc_after_for - SUCCESS\n";
+        std::cout << "SUCCESS\n";
     }
 }
 
 int main() {
-    linear_alloc_test();
+    // linear_alloc_test();
+
+    std::vector<int, linear_alloc<int>> v;
+    for(size_t i = 0; i < 100000; ++i)
+        v.push_back(i);
 
     return 0;
 }
